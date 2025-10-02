b56c647e78e795f3036a7a7ecddc0a464cc956c4
Doridian <git@doridian.net>
2025-10-02 13:02:25 -0700

    Add flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          default = pkgs.buildGoModule {
+            pname = "superfan";
+            version = "0.1.0";
+            src = ./.;
+            vendorHash = "sha256-E2AmHtnZ2+Q6vUXO4niMYdT+8saGTMMa4lEDkSB/jO4=";
+            buildInputs = [];
+          };
+        };
+      });
+}
