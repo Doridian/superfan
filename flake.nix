e57eeea587cdb715db5ad4c7c6b91591e5960dcc
Doridian <git@doridian.net>
2025-10-03 11:59:50 -0700

    Named package

--- flake.nix
+++ flake.nix
@@ -7,16 +7,18 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        package = pkgs.buildGoModule {
+          pname = "superfan";
+          version = "0.1.0";
+          src = ./.;
+          vendorHash = "sha256-E2AmHtnZ2+Q6vUXO4niMYdT+8saGTMMa4lEDkSB/jO4=";
+          buildInputs = [];
+        };
       in
       {
         packages = {
-          default = pkgs.buildGoModule {
-            pname = "superfan";
-            version = "0.1.0";
-            src = ./.;
-            vendorHash = "sha256-E2AmHtnZ2+Q6vUXO4niMYdT+8saGTMMa4lEDkSB/jO4=";
-            buildInputs = [];
-          };
+          default = package;
+          superfan = package;
         };
       });
 }
