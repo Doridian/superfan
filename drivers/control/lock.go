44cb80f99b2a8a15ec0447f0424c2f094f813550
Doridian <git@doridian.net>
2025-10-24 16:35:21 -0700

    Try to acquire ipmi lock for a bit

--- drivers/control/lock.go
+++ drivers/control/lock.go
@@ -2,33 +2,40 @@ package control
 
 import (
 	"errors"
+	"time"
 
 	"github.com/gofrs/flock"
 )
 
 var lockFile = flock.New("/var/lock/ipmi.lock")
+var LockTimeout = time.Second * 10
+var LockRetryInterval = time.Millisecond * 10
+
+func acquireLock() (err error) {
+	var locked bool
+	var lockWaitEnd = time.Now().Add(LockTimeout)
+	for time.Now().Before(lockWaitEnd) && !locked {
+		locked, err = lockFile.TryLock()
+		if err != nil || locked {
+			return
+		}
+		time.Sleep(LockRetryInterval)
+	}
+	return errors.New("could not acquire IPMI lock")
+}
 
 func RunLocked[T any](f func() (T, error)) (T, error) {
-	locked, err := lockFile.TryLock()
-	if err != nil {
+	if err := acquireLock(); err != nil {
 		var zero T
 		return zero, err
-	}
-	if !locked {
-		var zero T
-		return zero, errors.New("could not acquire IPMI lock")
 	}
 	defer lockFile.Unlock()
 	return f()
 }
 
 func RunLockedVoid(f func() error) error {
-	locked, err := lockFile.TryLock()
-	if err != nil {
+	if err := acquireLock(); err != nil {
 		return err
-	}
-	if !locked {
-		return errors.New("could not acquire IPMI lock")
 	}
 	defer lockFile.Unlock()
 	return f()
